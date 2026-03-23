0eb599e03f9d8a06ce6e838449ea44b5abd089d2
Nhat Le <nhatleminh1997@gmail.com>
2026-03-23 12:08:33 -0400

    "updated scoutmini_nav2 metadata"

--- ros2_ws/src/scoutmini_nav2/setup.py
+++ ros2_ws/src/scoutmini_nav2/setup.py
@@ -1,4 +1,5 @@
 from setuptools import find_packages, setup
+from glob import glob
 
 package_name = 'scoutmini_nav2'
 
@@ -8,14 +9,18 @@ setup(
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', glob('launch/*.launch.py')),
+        ('share/' + package_name + '/config', glob('config/*.yaml')),
+        ('share/' + package_name + '/maps', glob('maps/*')),
+        ('share/' + package_name + '/rviz', glob('rviz/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='nle',
     maintainer_email='nhatleminh1997@gmail.com',
-    description='TODO: Package description',
+    description='Nav2 bringup package for Scout Mini',
     license='Apache-2.0',
     extras_require={
         'test': [
