240740c37ee81bde718ceb414d93124fad600903
nle <nhatleminh1997@gmail.com>
2026-05-07 11:35:11 -0400

    fixed broken package setup

--- ros2_ws/src/scoutmini_tasks/setup.py
+++ ros2_ws/src/scoutmini_tasks/setup.py
@@ -1,12 +1,12 @@
 from glob import glob
-from setuptools import setup
+from setuptools import setup, find_packages
 
 package_name = 'scoutmini_tasks'
 
 setup(
     name=package_name,
     version='0.0.1',
-    packages=[package_name],
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
